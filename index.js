720784154efbb6ff9ec7413982ccd9877d18ed3b
راكينول إسلام <you@example.com>
2022-10-26 10:35:42 +0600

    data loaded

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-
+const allCourses = require('./data/course.json');
 
 app.get('/', (req, res) =>{
     res.send('api running')
 });
+
+app.get('/courses', (req, res) =>{
+    res.send(allCourses);
+})
 
 app.listen(port, () =>{
     console.log('server running on port', port);
